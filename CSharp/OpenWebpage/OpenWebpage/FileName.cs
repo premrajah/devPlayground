de114594f4b35435d5a79e372b629de0e9778b8c
Prem Rajah <prajah@ctlondon.com>
2018-03-01 10:06:19 +0000

    added CSharp files

---
+++ CSharp/OpenWebpage/OpenWebpage/FileName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWebpage
+{
+    class FileName
+    {
+        public string filename { get; set; }
+
+        public FileName() { }
+
+        public FileName(string filename)
+        {
+            this.filename = filename;
+        }
+    }
+}
---
+++ CSharp/OpenWebpage/OpenWebpage/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace OpenWebpage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string[] filenames = Directory.GetFiles(@"C:\xampp\htdocs\worksandbox\athletes28Feb2018\AthleticsHtmlDisplay\data")
+            .Select(System.IO.Path.GetFileName)
+            .ToArray();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                foreach (string file in filenames)
+                {
+                    if (file.Contains("_RESULT_LIST"))
+                    {
+                        Rfilename_cb.Items.Add(file.Substring(0,9));
+                    }
+
+                    if (file.Contains("_START_LIST"))
+                    {
+                        Sfilename_cb.Items.Add(file.Substring(0,9));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+            
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string RCode = $"?Code={ResultCode_tbox.Text}";
+            string RLocation = "http://localhost/worksandbox/athletes28Feb2018/AthleticsHtmlDisplay/RESULTS_LIST.html" + RCode;
+
+            try
+            {   
+                if(ResultCode_tbox.Text != String.Empty)
+                {
+                    System.Diagnostics.Process.Start(RLocation);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string SCode = $"?Code={StartCode_tbox.Text}";
+            string SLocation = "http://localhost/worksandbox/athletes28Feb2018/AthleticsHtmlDisplay/START_LIST.html" + SCode;
+
+            try
+            {
+                if(StartCode_tbox.Text != String.Empty)
+                {
+                    System.Diagnostics.Process.Start(SLocation);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
